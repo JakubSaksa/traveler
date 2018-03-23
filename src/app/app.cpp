68ec05de7d2d1c76b46c2dd742ee547f57874943
JakubSaksa <jakub.saksa@icloud.com>
2018-03-23 15:21:41 +0100

    demo

--- src/app/app.cpp
+++ src/app/app.cpp
@@ -269,6 +269,10 @@ void app::run_drawing(
         
         for(size_t i = 0; i < mapping.size(); ++i)
         {
+            
+            INFO("templated: %s", templated[i].print_tree(false));
+            INFO("matched: %s", matched[i].print_tree(false));
+            
             //Based on a mapping, matcher returns structure with deleted and inserted nodes
             // which correspond to the target structure
             if(templated[i].size() == 1 && matched[i].size() != 1) templated[i] = matched[i];
--- src/draw/compact.cpp
+++ src/draw/compact.cpp
@@ -195,9 +195,6 @@ void compact::init()
         iterator par = rna_tree::parent(it);
         point p = par->center();
         
-        INFO("%s", par -> initiated_points());
-        INFO("%s %s", p.x, p.y);
-        
         assert(!p.bad());
         
         if (rna_tree::is_root(par))
