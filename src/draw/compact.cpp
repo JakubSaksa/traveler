4bc8bdca624d270a1e4d520c75bab4fa42709c85
JakubSaksa <jakub.saksa@icloud.com>
2018-06-15 17:12:29 +0200

    alg

--- src/draw/compact.cpp
+++ src/draw/compact.cpp
@@ -947,8 +947,8 @@ double compact::get_length(
             auto pos = prev -> labels[0].p;
             for(auto&& l: it->labels)
             {
-                pos.x += 1;
-                pos.y += 1;
+                //pos.x += 1;
+                pos.y -= 10;
                 
                 INFO("x: %s, y: %s", pos.x, pos.y);
                 
